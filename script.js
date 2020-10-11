4f7ab0f6c483de49b731d19002a157d5863e9ea6
akashpundir931 <akashpundir931@gmail.com>
2020-10-11 23:18:48 +0530

    menubar change

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ var i = 0;
 var images = [];
 var time = 3000;
 
-images[0] = "img/slide1.png";
-images[1] = "img/slide2.png";
-images[2] = "img/res.jpg";
+images[0] = "img/1.png";
+images[1] = "img/2.png";
+images[2] = "img/3.png";
+images[2] = "img/4.png";
 
 function changeImg(){
     document.slide.src = images[i];
